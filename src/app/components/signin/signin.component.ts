07d4eaba307b9c79b25a6505b672ca3ae4256c40
kevdesign98 <kevin.cooray98@gmail.com>
2026-05-10 18:04:42 +0200

    Add NavbarSignin component and include in Signin
    Introduce a standalone NavbarSignin component (component, template, CSS, and unit spec) that uses Angular signals and injects AuthService and Router to manage nav links, profile dropdown, mobile menu, and logout behavior. Integrate the new <app-navbar-signin/> into the Signin page and add the component to the Signin imports. Files added: navbar-signin.component.ts, .html, .css, .spec.ts; files modified: signin.component.html, signin.component.ts.

--- src/app/components/signin/signin.component.ts
+++ src/app/components/signin/signin.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../core/auth.service';
 import { FooterComponent } from '../footer/footer.component';
+import { NavbarSigninComponent } from '../navbar-signin/navbar-signin.component';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [FooterComponent],
+  imports: [FooterComponent, NavbarSigninComponent],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
